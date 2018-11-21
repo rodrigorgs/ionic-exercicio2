04038db7ca0fa9281cb0e645b92e69eb0133a8b1
Rodrigo Rocha <rodrigorgs@gmail.com>
2018-11-21 19:57:35 -0300

    Adiciona especificação do exercício

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,15 +7,63 @@ describe('App', () => {
     page = new Page();
   });
 
-  describe('default screen', () => {
+  describe('página inicial', () => {
     beforeEach(() => {
       page.navigateTo('/');
     });
 
-    it('should have a title saying Page One', () => {
-      page.getPageOneTitleText().then(title => {
-        expect(title).toEqual('Page One');
+    // checa valores iniciais da 1a tela
+    // checa valores iniciais da 2a tela
+
+    it('deve cumprimentar Fulano', () => {
+      page.getTexto().then(texto => {
+        expect(texto).toEqual('Oi, Fulano!');
       });
+    });
+
+    it('deve trocar nome ao confirmar', () => {
+      page.trocar()
+          .then(() => page.digitar(' de Tal'))
+          .then(() => page.confirmar())
+          .then(() => page.sleep(500))
+          .then(() => page.getTexto())
+          .then((texto) => {
+            expect(texto).toEqual('Oi, Fulano de Tal!');
+          });
+    });
+
+    it('não deve trocar nome ao cancelar', () => {
+      page.trocar()
+          .then(() => page.digitar(' de Tal'))
+          .then(() => page.cancelar())
+          .then(() => page.sleep(500))
+          .then(() => page.getTexto())
+          .then((texto) => {
+            expect(texto).toEqual('Oi, Fulano!');
+          });
+    });
+
+    it('deve dizer apenas "Oi!" quando usuário estiver em branco', () => {
+      page.trocar()
+          .then(() => page.limpar())
+          .then(() => page.confirmar())
+          .then(() => page.sleep(500))
+          .then(() => page.getTexto())
+          .then((texto) => {
+            expect(texto).toEqual('Oi!');
+          });
+    });
+
+    it('deve mostrar o nome atual na caixa de texto', () => {
+      page.trocar()
+          .then(() => page.digitar(' de Tal'))
+          .then(() => page.confirmar())
+          .then(() => page.sleep(500))
+          .then(() => page.trocar())
+          .then(() => page.getValorDoInput())
+          .then((texto) => {
+            expect(texto).toEqual('Fulano de Tal');
+          });
     });
   })
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 export class Page {
 
@@ -13,4 +13,41 @@ export class Page {
   getPageOneTitleText() {
     return element(by.tagName('page-page1')).element(by.tagName('ion-title')).element(by.css('.toolbar-title')).getText();
   }
+
+  getTexto() {
+    return element(by.id('textView')).getText();
+  }
+  getValorDoInput() {
+    return element(by.css('#editText .text-input')).getAttribute('value')
+  }
+  trocar() {
+    return element(by.id('btnTrocar')).click();
+  }
+  digitar(str: string) {
+    return browser.sleep(500)
+    .then(() => {
+      element(by.css('#editText .text-input')).sendKeys(str)
+    }).then(() => {
+      browser.sleep(200);
+    });
+  }
+  limpar() {
+    return browser.sleep(500)
+        .then(() => this.getValorDoInput())
+        .then((texto) => {
+          let array = Array(texto.length + 1).fill(protractor.Key.BACK_SPACE);
+          return element(by.css('#editText .text-input')).sendKeys(...array);
+        })
+        .then(() => browser.sleep(500));
+  }
+  confirmar() {
+    return element(by.id('btnConfirmar')).click();
+  }
+  cancelar() {
+    return element(by.id('btnCancelar')).click();
+  }
+  sleep(ms: number) {
+    return browser.sleep(ms);
+  }
+
 }
